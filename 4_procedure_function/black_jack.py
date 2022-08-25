a2575e27270826aec28e5f067c75eb2db90d31a0
AyatoMatsui <77559448+O01o@users.noreply.github.com>
2022-08-26 03:12:20 +0900

    trump updated

--- 4_procedure_function/black_jack.py
+++ 4_procedure_function/black_jack.py
@@ -2,3 +2,6 @@ import trump
 
 trump_card = trump.trump_card
 print(trump.trump_printer(trump_card))
+
+player = []
+enemy = []
--- 4_procedure_function/trump.py
+++ 4_procedure_function/trump.py
@@ -8,6 +8,27 @@ def trump_printer(trump_card):
     for i in trump_card:
         print(i)
 
+def hit_suit(input_suit):
+    count = 0
+    for i in suit:
+        if input_suit == i: return count
+        count += 1
+    print("not found suit")
+    exit()
+
+def hit_number(input_number):
+    count = 0
+    for i in number:
+        if input_number == i: return count
+        count += 1
+    print("not found number")
+    exit()
+
+def trump_drow(trump_card, suit, number):
+    trump_card[hit_suit(suit)][hit_number(number)] = 0
+    your_card = suit, number
+    return trump_card, your_card
+
 for i in suit:
     row = []
     for j in number:
