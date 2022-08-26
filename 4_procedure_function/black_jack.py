089fd4fff8f28d1545462c0442e3771e821357bb
AyatoMatsui <77559448+O01o@users.noreply.github.com>
2022-08-26 15:40:44 +0900

    main routine

--- 4_procedure_function/black_jack.py
+++ 4_procedure_function/black_jack.py
@@ -50,101 +50,103 @@ def point_calc(card_list):
             if sum <= 21: break
     return sum
 
-print("your turn")
-for i in range(2):
-    if len(trump.trump_card) > 0:
-        player_card.append(trump.trump_drow())
-    else: break
-for i in range(2):
-    if len(trump.trump_card) > 0:
-        cpu_card.append(trump.trump_drow())
-    else: break
-
-'''
-print("your card list")
-trump.trump_printer2(player_card)
-
-print("cpu card list")
-trump.trump_printer2(cpu_card)
-
-print("public card list")
-trump.trump_printer()
-'''
-
-print(len(player_card))
-print(len(cpu_card))
-
-'''
-point = point_calc([
-    {
-        'suit': 'heart',
-        'number': 'A'
-    },
-    {
-        'suit': 'diamond',
-        'number': 'A'
-    },
-    {
-        'suit': 'crover',
-        'number': 'A'
-    },
-    {
-        'suit': 'speed',
-        'number': 'A'
-    },
-    {
-        'suit': 'speed',
-        'number': '5'
-    },
-])
-print(point)
-'''
-
-'''
-player turn
-'''
-while True:
-    print('現在のあなたのカード')
-    trump.trump_printer2(player_card)
-    print('現在の合計得点:', point_calc(player_card))
-    print("カードを引きますか？: (y/N)")
-    text = input()
-    if text == 'y':
+        
+if __name__ == '__main__':
+    print("your turn")
+    for i in range(2):
         if len(trump.trump_card) > 0:
             player_card.append(trump.trump_drow())
-    else:
-        break
-
-'''
-dealer turn
-'''
-
-while True:
-    print('現在のCPUのカード')
-    trump.trump_printer2(cpu_card)
-    print('現在の合計得点:', point_calc(cpu_card))
-    
-    if point_calc(cpu_card) >= 17:
-        break
-    else:
+        else: break
+    for i in range(2):
         if len(trump.trump_card) > 0:
             cpu_card.append(trump.trump_drow())
+        else: break
+
+    '''
+    print("your card list")
+    trump.trump_printer2(player_card)
+
+    print("cpu card list")
+    trump.trump_printer2(cpu_card)
+
+    print("public card list")
+    trump.trump_printer()
+    '''
+
+    print(len(player_card))
+    print(len(cpu_card))
+
+    '''
+    point = point_calc([
+        {
+            'suit': 'heart',
+            'number': 'A'
+        },
+        {
+            'suit': 'diamond',
+            'number': 'A'
+        },
+        {
+            'suit': 'crover',
+            'number': 'A'
+        },
+        {
+            'suit': 'speed',
+            'number': 'A'
+        },
+        {
+            'suit': 'speed',
+            'number': '5'
+        },
+    ])
+    print(point)
+    '''
+
+    '''
+    player turn
+    '''
+    while True:
+        print('現在のあなたのカード')
+        trump.trump_printer2(player_card)
+        print('現在の合計得点:', point_calc(player_card))
+        print("カードを引きますか？: (y/N)")
+        text = input()
+        if text == 'y':
+            if len(trump.trump_card) > 0:
+                player_card.append(trump.trump_drow())
+        else:
+            break
+
+    '''
+    dealer turn
+    '''
+
+    while True:
+        print('現在のCPUのカード')
+        trump.trump_printer2(cpu_card)
+        print('現在の合計得点:', point_calc(cpu_card))
+        
+        if point_calc(cpu_card) >= 17:
+            break
+        else:
+            if len(trump.trump_card) > 0:
+                cpu_card.append(trump.trump_drow())
 
 
-print("最終的なあなたの得点: ", point_calc(player_card))
-print("最終的なディーラーの得点: ", point_calc(cpu_card))
+    print("最終的なあなたの得点: ", point_calc(player_card))
+    print("最終的なディーラーの得点: ", point_calc(cpu_card))
 
-if point_calc(player_card) > 21:
-    if point_calc(cpu_card) > 21:
-        print("ドローです")
-    else:
-        print("ディーラーの勝ちです")
-elif point_calc(cpu_card) > 21:
-    print("プレイヤーの勝ちです")
-else:
-    if point_calc(player_card) == point_calc(cpu_card):
-        print("ドローです")
-    elif point_calc(player_card) > point_calc(cpu_card):
+    if point_calc(player_card) > 21:
+        if point_calc(cpu_card) > 21:
+            print("ドローです")
+        else:
+            print("ディーラーの勝ちです")
+    elif point_calc(cpu_card) > 21:
         print("プレイヤーの勝ちです")
     else:
-        print("ディーラーの勝ちです")
+        if point_calc(player_card) == point_calc(cpu_card):
+            print("ドローです")
+        elif point_calc(player_card) > point_calc(cpu_card):
+            print("プレイヤーの勝ちです")
+        else:
+            print("ディーラーの勝ちです")
