dd244db95d31285af9a5a5b5f590caec7c9e9e8f
AyatoMatsui <77559448+O01o@users.noreply.github.com>
2022-09-16 17:31:38 +0900

    added image asset

---
+++ 3_iteration_list/opencv_tutorial1.py
@@ -0,0 +1,15 @@
+import cv2
+import numpy as np
+'''
+ここでのimread関数に入力するフォルダパスは、
+opencv_tutorial.pyから見た相対パスではなく、
+コマンド入力するターミナルのパスを見て判断します。
+パスの入力に注意しましょう。
+(あるいは絶対パスを打ってもOKです。)
+'''
+
+src = cv2.imread("./img/Lenna.bmp")
+
+cv2.imshow('output_image', src)
+cv2.waitKey()
+cv2.destroyAllWindows()
